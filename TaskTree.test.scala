cb5f8cdfc2f2f20bfcb9611b8052866a2875e11b
MercurieVV <mercurievv@gmail.com>
2026-07-29 01:29:26 +0300

    Repair failing Pull Request check for task #54

--- TaskTree.test.scala
+++ TaskTree.test.scala
@@ -1,21 +1,21 @@
 package com.github.mercurievv.ghllm
 
-import higherkindness.droste._
-import higherkindness.droste.data._
-import higherkindness.droste.syntax.all._
+import higherkindness.droste.data.Mu
+import higherkindness.droste.syntax.mu._
+import higherkindness.droste.Algebra
+import com.github.mercurievv.ghllm.task.TaskF
+import com.github.mercurievv.ghllm.task.TaskF._
 
 class TaskTreeSuite extends munit.FunSuite {
 
   test("build a 3-node tree and fold it to a node count with cata") {
-    import com.github.mercurievv.ghllm.TaskTree.{given, _}
-
-    val leaf: Fix[TaskF] = Fix(TaskF.Branch("leaf", Nil))
-    val left: Fix[TaskF] = Fix(TaskF.Branch("left", List(leaf)))
-    val tree: Fix[TaskF] = Fix(TaskF.Branch("root", List(left)))
+    val leaf = Mu[TaskF](Branch("leaf", Nil))
+    val left = Mu[TaskF](Branch("left", List(leaf)))
+    val tree = Mu[TaskF](Branch("root", List(left)))
 
     val countAlg: Algebra[TaskF, Int] = Algebra {
-      case TaskF.Branch(_, kids) => kids.sum + 1
-      case TaskF.Leaf(_)         => 1
+      case Branch(_, kids) => kids.sum + 1
+      case Leaf(_)         => 1
     }
 
     val total = tree.cata(countAlg)
--- TaskTreeTest.test.scala
+++ TaskTreeTest.test.scala
@@ -1,24 +1,21 @@
 package com.github.mercurievv.ghllm
 
-import higherkindness.droste.{Algebra, scheme}
-import higherkindness.droste.data.Fix
-import higherkindness.droste.syntax.all._
-import com.github.mercurievv.ghllm.TaskTree.TaskF
+import higherkindness.droste.data.Mu
+import higherkindness.droste.syntax.mu._
+import higherkindness.droste.Algebra
+import com.github.mercurievv.ghllm.task.TaskF
+import com.github.mercurievv.ghllm.task.TaskF._
 
 class TaskTreeTest extends munit.FunSuite {
 
   test("3-node tree folds to node count with cata") {
-    // Two leaf nodes.
-    val leafA = Fix(TaskF.Branch("a", Nil))
-    val leafB = Fix(TaskF.Branch("b", Nil))
-    // Root with two children → 3 nodes total.
-    val root  = Fix(TaskF.Branch("root", List(leafA, leafB)))
+    val leafA = Mu[TaskF](Branch("a", Nil))
+    val leafB = Mu[TaskF](Branch("b", Nil))
+    val root  = Mu[TaskF](Branch("root", List(leafA, leafB)))
 
-    // Algebra: each node contributes 1 + sum of children counts.
     val countAlgebra: Algebra[TaskF, Int] = Algebra {
-      case TaskF.Branch(_, children) =>
-        children.sum + 1
-      case TaskF.Leaf(_) => 1
+      case Branch(_, children) => children.sum + 1
+      case Leaf(_)             => 1
     }
 
     val total = root.cata(countAlgebra)
